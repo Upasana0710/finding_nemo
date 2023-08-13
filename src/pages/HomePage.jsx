b214a9a2add0155e9a4aad5fb6d6dc85532cbc39
Akangkha <akangkhasarkar@gmail.com>
2023-08-13 20:22:13 +0530

    Resolved conflicts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
+
 import React from 'react';
 import logo from '../assets/logo.jpg';
 import '../styles/Home.css'
+import HomeCard from '../Components/HomeCard';
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <div ><img src={logo} className="icon" width="100px"/></div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#019aff" fill-opacity="1" d="M0,64L48,64C96,64,192,64,288,101.3C384,139,480,213,576,234.7C672,256,768,224,864,224C960,224,1056,256,1152,229.3C1248,203,1344,117,1392,74.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-    </div>
-  )
+    <div className='HomeScreen'>
+      {/* <div ><img src={logo} className="icon" width="100px"/></div> */}
+      <HomeCard/>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,32L48,32C96,32,192,32,288,69.3C384,107,480,181,576,213.3C672,245,768,235,864,208C960,181,1056,139,1152,122.7C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+       </div> )
 }
+
 
 export default HomePage;
