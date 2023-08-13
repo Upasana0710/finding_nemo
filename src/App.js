213ae37325cde9a39c33d8efb83bd0e502f59684
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-08-13 22:17:27 +0530

    Removed unused module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
-import './Components/FoundCard';
-import FoundPage from './pages/FoundPage';
-import LostPage from './pages/LostPage';
-import HomePage from './pages/HomePage'
-import './App.css'
+import "./Components/FoundCard";
+import FoundPage from "./pages/FoundPage";
+import LostPage from "./pages/LostPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
     <div className="screen">
       <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="Founditems" element={<FoundPage />}/> 
-            <Route path="Lostitems" element={<LostPage />}/> 
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="Founditems" element={<FoundPage />} />
+          <Route path="Lostitems" element={<LostPage />} />
         </Routes>
-        </Router>
+      </Router>
     </div>
-  )}
-
+  );
+}
 
 export default App;
