a52bca0ebbe094fa51d2787b069326fb818ebe13
Akangkha <akangkhasarkar@gmail.com>
2023-08-14 09:11:34 +0530

    Further changes in the homescreen

--- src/Components/HomeCard.jsx
+++ src/Components/HomeCard.jsx
@@ -3,18 +3,17 @@ import "../styles/Home.css";
 import Button from "../Components/Button";
 import img from "../assets/l&f.png";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/Group.png";
 const HomeCard = () => {
   return (
     <div className="HomeCard">
     <div className="Cardimg">
-        <img src={img} width="60%" />
+        <img src={img} width="100%" draggable="false"/>
     </div>
       <div className="Cardb">
-      <img src={logo} width="30%" />
+      <img src={logo} width="100%" draggable="false" />
         <div className="hbtn">
-        <Link to="/Founditems" style={{ textDecoration: 'none' }}><Button button="Item Found" /></Link>
-        <Link to="/Lostitems" style={{ textDecoration: 'none' }}><Button button="Items Lost" /></Link>
+        <Link to="/Lostitems" style={{ textDecoration: 'none' }}><Button button="Embark on the journey!" className="homeButton"/></Link>
         </div>
       </div>
     </div>
